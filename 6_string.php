6210df9cb5fc1b9f43c7813964e7db1944d1a5b6
MH Miyazi <83886011+mhmiyazi@users.noreply.github.com>
2021-09-13 12:57:22 +0600

    Create 6_string.php

---
+++ 6_string.php
@@ -0,0 +1,11 @@
+<?php
+  //A string is a sequence of characters, like "Hello world!".
+  //functions to manipulate strings
+  
+    echo strlen("Hello world!"); // outputs 12
+    echo str_word_count("Hello world!"); // outputs 2
+    echo strrev("Hello world!"); // outputs !dlrow olleH
+    echo strpos("Hello world!", "world"); // outputs 6
+    echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+
+?>
