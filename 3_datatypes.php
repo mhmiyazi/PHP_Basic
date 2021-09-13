cc3987a47bbce435116b791d5010e8202683aefe
MH Miyazi <83886011+mhmiyazi@users.noreply.github.com>
2021-09-13 12:51:16 +0600

    Create 3_datatypes.php

---
+++ 3_datatypes.php
@@ -0,0 +1,9 @@
+<?php
+//dt = DataTypes
+$string_dt = "Hello world!";
+$integer_dt = 23;
+$float_dt = 23.4
+$null_dt = null;
+$boolean_dt = TRUE;
+$array_dt = array("One","Two","Three","Four","Five");
+?>
